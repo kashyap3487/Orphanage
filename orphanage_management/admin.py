a9ef281fdd6885558bf5010f3fd82c239746032e
kashyap3487 <44424588+kashyap3487@users.noreply.github.com>
2021-12-28 10:29:04 -0500

    Add files via upload

---
+++ orphanage_management/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Donor)
+admin.site.register(Child)
+admin.site.register(Review)
+admin.site.register(Contact)
+# admin.site.register(City)
+admin.site.register(Volunteer)
---
+++ orphanage_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrphanageManagementConfig(AppConfig):
+    name = 'orphanage_management'
